201de1d599d877c2da257843b409188dd9f6a329
Naoto Suzuki <crappy.naoto@gmail.com>
2018-12-05 01:19:34 +0900

    indexの配列追加

--- index.php
+++ index.php
@@ -4,6 +4,41 @@ require_once("grammer_class.php");
 ini_set('display_errors', 0);
 ini_set('display_errors', 1);
 
+class Index{
+  private $grammer;
+
+  public function __construct($grammer,$grammerValue){
+    $this->grammers=$grammer;
+    $this->grammerValue=$grammerValue;
+  }
+
+  public function getGrammer(){
+    return $this->grammer=$grammer;
+  }
+
+  public function getGrammerValue(){
+    return $this->grammerValue=$grammerValue;
+  }
+
+  $beverb=new Index('Be動詞' );
+  $verb=new Index('一般動詞' );
+  $pronoun=new Index('代名詞' );
+  $thirdPerson=new Index('三人称単数' );
+  $can=new Index('Can' );
+
+  $grammers=array($beverb,$verb,$pronoun,$thirdPerson,$can);
+
+  $beverbValue=new Index('beverb' );
+  $verbValue=new Index('verb' );
+  $pronounValue=new Index('pronoun' );
+  $thirdPersonValue=new Index('thirdperson' );
+  $canValue=new Index('can' );
+
+  $grammerValues=array($beverbValue,$verbValue,$pronounValue,$thirdPersonValue,$canValue);
+
+
+}
+
 ?>
 
 <HTMl>
@@ -21,12 +56,14 @@ ini_set('display_errors', 1);
         </header>
 
         <div class="container">
-          <?php foreach($grammers as $grammer):?>
+          <?php foreach($grammerValues as $grammerValue):?>
             <!--DBから持ってくるか配列を作るか・・・配列を作ろう-->
                 <div class="item">
-                  <a href="grammer.php?name=<?php echo $grammer->getValue() ?>">
+                  <a href="grammer.php?name=<?php echo $grammerValue->getGrammerValue() ?>">
+          <?php endforeach?>
                     <!--grammer_class_phpのリストを参考に連想配列を作り、keyとvalueをそれぞれechoさせる。他に方法あるだろうけど-->
-                    <?php echo $grammer->getName() ?>
+                  <?php foreach($grammers as $grammer):?>
+                    <?php echo $grammer->getGrammer() ?>
                   </a>
                 </div>
           <?php endforeach?>
