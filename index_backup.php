31d5fc4d1405cd260411452b122d2321b897e661
Naoto Suzuki <crappy.naoto@gmail.com>
2018-12-08 11:42:18 +0900

    indexにindex_testを適用

--- index_backup.php
+++ index_backup.php
@@ -1,16 +1,45 @@
 <?php
+// require_once("grammer_class.php");
 // エラー表示なし
 ini_set('display_errors', 0);
 ini_set('display_errors', 1);
 
+class Index{
+  private $grammer;
+  private $grammerValue;
 
+  public function construct($grammer){
+    $this->grammer=$grammer;
+  }
+
+  public function getGrammer(){
+    return $this->grammer;
+  }
+
+  // public function getGrammerValue(){
+  //   return $this->grammerValue;
+  // }
+
+}
+
+$beverb=new Index('Be動詞');
+$verb=new Index('一般動詞');
+$pronoun=new Index('代名詞');
+$thirdPerson=new Index('三人称単数');
+$can=new Index('Can');
+
+
+$grammerIndicate=array($beverb=>"beverb",$verb=>"verb",$pronoun=>"pronoun",$thirdPerson=>"thirdperson",$can="can");
 
 // $beverbValue=new Index('beverb');
 // $verbValue=new Index('verb');
 // $pronounValue=new Index('pronoun');
 // $thirdPersonValue=new Index('thirdperson');
 // $canValue=new Index('can');
-  $grammerIndicate=array("be動詞"=>"beverb","単数形,複数形"=>"singular_plural","一般動詞"=>"verb","代名詞"=>"pronoun","三人称"=>"thirdperson","Can"=>"can");
+
+
+
+
 ?>
 
 <HTMl>
@@ -28,12 +57,10 @@ ini_set('display_errors', 1);
         </header>
 
         <div class="container">
-          <?php
-
-          foreach($grammerIndicate as $grammer=>$grammerValue):?>
+          <?php foreach($grammeerIndicate as $grammer=>$grammerValue):?>
                 <div class="item">
                   <a href="grammer.php?name=<?php echo $grammerValue ?>">
-                    <p><?php echo $grammer ?></p>
+                    <p><?php echo $grammer->getGrammer() ?></p>
                   </a>
                 </div>
               <?php endforeach?>
