10b036b5b3173bf6475603999cf53a5103576291
Naoto Suzuki <crappy.naoto@gmail.com>
2019-01-28 21:58:21 +0900

    回答内容表示完了：postが渡らんのは、配列に入ってるのにそれぞれのポスト名になるようにしてかったので、空の値が最後に上書きされてたからでした

--- answer.php
+++ answer.php
@@ -1,65 +1,112 @@
 <?php
-ini_set('display_errors', 0);
-ini_set('display_errors', 1);
-//require_once("question_class.php");
-require_once("answer_class.php");
-require_once("pdo_class.php");
-$grammerName=$_POST['name'];
-$pdo = new mysqlClass();
+session_start();
+$genre_param = $_GET['name'];
+ini_set(‘display_errors’, 1);
+var_dump($_POST);
 
-$records = $pdo->getAnswerRecord($grammerName);
-$answerclass = new Answer($records);
+
+$sql = null;
+$res = null;
+$dbh = null;
+
+
+try {
+	// DBへ接続
+	$dbh = new PDO("mysql:host=localhost; dbname=beyou; charset=utf8", 'test', 'test');
+
+	// SQL作成
+	$sql = "SELECT genres.genre_value, big_questions.id as bigID, big_questions.question as bigQuestion, small_questions.question_num ,small_questions.question as smallQuestion, small_questions.answer as answer FROM big_questions INNER JOIN small_questions ON big_questions.id = small_questions.big_questions_id INNER JOIN genres ON small_questions.genre_value = genres.genre_value order by bigID asc, question_num asc;";
+	// SQL実行
+	$res = $dbh->query($sql);
+	$records = $res->fetchAll(PDO::FETCH_ASSOC);
+
+
+	$big_pares =array_column($records, "bigQuestion","bigID");
+
+
+
+	$small_pares = array_column($records,"smallQuestion");
+	// var_dump($small_pares);
+
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+	die();
+}
 
 
 ?>
 
-<!--問題と答えをPDOで持ってきて、回答内容をquestion.phpから持ってくる -->
-
 <HTMl>
 
-    <head>
-        <meta charset="utf-8">
-        <title>Be.you</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
-    <body>
+	<head>
+		<meta charset="utf-8">
+		<title>Be.you</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+
         <form action="" method="post">
-            <?php  $i=0; foreach ($answerclass->getAnswer() as $answer):$j=1;?>
-                <div>
-                    <!-- <?php var_dump($answer) ?> -->
-                    <?php foreach ($answer as $key=>$answerValue):?>
-                        <p><?php
-                        if ($key == "question"){
-                            $i++;
-                            echo "Q".$i." ".$answerValue ;
-                        } else {
-                            $sentence = "sentence_" . $j;
-                            // var_dump($key);
-                            if ($key == $sentence){
-                                echo "(".$j.")".$answerValue ;
-                                // echo "</br>";
-                                // var_dump("sentence");
-                            } else {
-                                echo $answerValue ;
-                                echo "</br>";
-                                echo '<p>', $_POST[strval($i).strval($j)] ,'</p>';
-                                $j++;
-                                // var_dump("answer");
-                            }
 
-                                // このコードをanswerDBに対応させる
-                        }
-                            ?></p>
-                            <?//php endforeach ?>
-                        <?php endforeach ?>
-                    </div>
-                    <?//php endforeach ?>
-                <?php endforeach ?>
-                <input type="hidden" name="name" value = "<?php echo $grammerName;?>"></input>
-            </form>
-            <a href="explain.php?name=<?php echo $grammerName;?>">解説を読む</a>
+		<?php $bigQ_flag = 1 ?>
+		<!-- フラグと大問idが一致すれば、その大問に属する小問題を全て展開し、小問題を全て展開し終わったら次の配列に進むって感じにする -->
+
+		<?php foreach ($records as $question_arrays): ?>
+
+            <br>
+            <br>
+
+			<?php if($genre_param == $question_arrays['genre_value']): ?>
+				<?php $bigQ_flag = $question_arrays['bigID']?>
+
+				<?php foreach ($big_pares as $bigID => $bigQuestion): ?>
+					<?php if($bigQ_flag == $bigID): ?>
+						<?php echo "Q".$bigID.".". $bigQuestion ?>
+					<?php endif ?>
 
 
-        </body>
+				<?php foreach($records as $smalls): ?>
+                    <?php if($bigQ_flag==$smalls['bigID']): ?>
+						<br>
+						<br>
+						<?php echo "(".$smalls['question_num'].")".$smalls['smallQuestion']."<br/>"."答え：".$smalls['answer'] ?>
+                        <br>
+                        <?php echo "あなたの回答:".$_POST["user_answer_".$smalls['bigID'].$smalls['question_num']]?>
 
-    </HTMl>
+                        <br/>
+                        <input type="checkbox" name="result" value="0">正解<br/>
+                        <input type="checkbox" name="result" value="1">不正解<br/>
+					<?php  endif ?>
+
+
+				<?php  endforeach ?>
+
+				<?php $bigQ_flag++ ?>
+				<br>
+				<br>
+
+			<?php endforeach ?>
+			<?php break ?>
+			<?php endif ?>
+
+		<?php endforeach ?>
+
+
+
+
+
+
+
+
+			<input type="submit" value="結果を保存する(復習の参考にできます！)" />
+			<input type="hidden" name="name" value = "<?php echo $genre_param;?>"/>
+
+		</form>
+
+		<a href="explain.php?name=<?php echo $genre_param;?>">解説を読む</a>
+
+
+	</body>
+
+</HTMl>
--- question.php
+++ question.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+ini_set(‘display_errors’, 1);
+echo $_SESSION['ID'];
 $genre_param = $_GET['name'];
 
 
@@ -8,32 +10,7 @@ $res = null;
 $dbh = null;
 
 
-try {
-	// DBへ接続
-	$dbh = new PDO("mysql:host=localhost; dbname=beyou; charset=utf8", 'test', 'test');
 
-	// SQL作成
-	$sql = "SELECT genres.genre_value, big_questions.id as bigID , big_questions.question as bigQuestion, small_questions.question_num ,small_questions.question as smallQuestion FROM big_questions INNER JOIN small_questions ON big_questions.id = small_questions.big_questions_id INNER JOIN genres ON small_questions.genre_value = genres.genre_value order by bigID asc, question_num asc;";
-	// SQL実行
-	$res = $dbh->query($sql);
-	$records = $res->fetchAll(PDO::FETCH_ASSOC);
-
-	$big_pares =array_column($records, "bigQuestion","bigID");
-	var_dump($big_pares);
-
-	echo "<br/>";
-	echo "<br/>";
-
-
-
-	$small_pares = array_column($records,"smallQuestion");
-	var_dump($small_pares);
-
-
-} catch(PDOException $e) {
-	echo $e->getMessage();
-	die();
-}
 
 try {
 	// DBへ接続
@@ -46,10 +23,6 @@ try {
 	$records = $res->fetchAll(PDO::FETCH_ASSOC);
 
 	$big_pares =array_column($records, "bigQuestion","bigID");
-	// var_dump($big_pares);
-
-	echo "<br/>";
-	echo "<br/>";
 
 
 
@@ -68,25 +41,24 @@ try {
 <HTMl>
 
 	<head>
-		<meta charset="utf-8">
+		<meta charset="UTF-8">
 		<title>Be.you</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
 	<body>
-		<br>
-		<br>
-		<br>
+		<form action="answer.php?name=<?php echo $genre_param ?>" method="post">
+
 		<?php $bigQ_flag = 1 ?>
 		<!-- フラグと大問idが一致すれば、その大問に属する小問題を全て展開し、小問題を全て展開し終わったら次の配列に進むって感じにする -->
 
-		<?php foreach ($records as $question_arrays): echo $bigQ_flag;?>
+		<?php foreach ($records as $question_arrays): ?>
 			<?php if($genre_param == $question_arrays['genre_value']): ?>
 				<?php $bigQ_flag = $question_arrays['bigID']?>
 
 				<?php foreach ($big_pares as $bigID => $bigQuestion): ?>
 					<?php if($bigQ_flag == $bigID): ?>
-						<?php echo $bigQuestion ?>
+						<?php echo "Q".$bigID.".". $bigQuestion ?>
 					<?php endif ?>
 
 
@@ -94,32 +66,32 @@ try {
 					<?php if($bigQ_flag==$smalls['bigID']): ?>
 						<br>
 						<br>
-						<?php echo $smalls['smallQuestion'] ?>
-					<?php endif ?>
-				<?php endforeach ?>
+						<?php echo "(".$smalls['question_num'].")".$smalls['smallQuestion'] ?><br>
+						<input type="text" name="user_answer.<?php echo $smalls['bigID'].$smalls['question_num'] ?>"></input>
+
+
+					<?php  endif ?>
+
+				<?php  endforeach ?>
 
 				<?php $bigQ_flag++ ?>
-				<?php echo $bigQ_flag ?>
 				<br>
 				<br>
-
-
+				<br>
+				<br>
 			<?php endforeach ?>
 			<?php break ?>
 			<?php endif ?>
 
-			<br>
 		<?php endforeach ?>
 
 
 
-
-		<form action="answer.php" method="post">
-
-
+			 <br>
+			 <input type="hidden" name="name" value = "<?php echo $genre_param;?>"/>
 
 			<input type="submit" value="答え合わせをする" />
-			<input type="hidden" name="name" value = "<?php echo $genre_param;?>"/>
+
 		</form>
 		<a href="explain.php?name=<?php echo $genre_param;?>">解説を読む</a>
 
