9578c8459a0b3d73d3c462c6dc24a76fbdeeb471
Naoto Suzuki <crappy.naoto@gmail.com>
2019-02-24 15:17:31 +0900

    処理を分けるためにfunctions.phpを作成

--- answer_class.php
+++ answer_class.php
@@ -1,18 +1,19 @@
 <?php
-
-class Answer{
-  private $answer;
-
-  public function __construct($answer){
-    $this->answer=$answer;
-  }
-
-  public function getAnswer(){
-    return $this->answer;
-  }
-
+function user_answer_indicate(){
+    foreach($small_records as $record_value){
+        $big_que=$record_value["big_questions_id"];
+        $big_q=$record_value["big_question"];
+        $small_q=$record_value["question"];
+        $small_a=$record_value["answer"];
+        $questions1[$big_que]=["big_question"=>$big_q];
+        $questions2[$big_que][]="$small_q";
+        $answers[$big_que][]="$small_a";
+    }
+    for($i=1; $i<=3; $i++ ){
+        $questions[$i]=$questions1[$i];
+        $questions[$i]["questions"]=$questions2[$i];
+        $questions[$i]["answers"]=$answers[$i];
+    }
 }
-
-
 
 ?>
--- answer_model.php
+++ answer_model.php
@@ -1,6 +1,7 @@
 
 <?php
 require_once("pdo_class.php");
+
 function answerGetRecord($genre_param){
     $dbh = new PdoClass();
     try {
@@ -30,4 +31,60 @@ function answerGetRecord($genre_param){
     }
     $records = [$big_records,$small_records];
     return $records;
+}
+
+//function.phpのformUserResultから$answer_datasを受け取る必要がある
+function insertUserAnser($user_datas){
+    try {
+        $pdo = new PDO("mysql:host=localhost; dbname=beyou; charset=utf8", 'test', 'test');
+        $answer_sql = "INSERT INTO users_answer (
+            user_id,
+            genre_value,
+            big_questions_id,
+            question_num,
+            user_answer,
+            result,
+            created
+        ) VALUES (
+            :user_id,
+            :genre_value,
+            :big_questions_id,
+            :question_num,
+            :user_answer,
+            :result,
+            now()
+        )";
+        foreach ($answer_datas as $answer_data){
+            // $stmt = $dbh->prepare();
+            // $answer_bind_array = null;
+            // $answer_bind_array = array(
+            //     "user_id" => $user_id,
+            //     "genre_value" => $genre_param,
+            //     "big_questions_id" => $answer_data["big_questions_id"],
+            //     "question_num" => $answer_data["question_num"],
+            //     "user_answer" => $answer_data["user_answer"],
+            //     "result" => $answer_data["result"],
+            // );
+        $stmt=$pdo->prepare($answer_sql);
+        // $stmt->bindParam($answer_bind_array);
+        // var_dump($answer_sql);
+        $big_ID=$answer_data["big_questions_id"];
+            $question_num=$answer_data["question_num"];
+            $user_answer=$answer_data["user_answer"];
+            $result=$answer_data["result"];
+
+            $stmt->bindParam(":user_id", $user_id);
+            $stmt->bindParam(":genre_value", $genre_param);
+            $stmt->bindParam(":big_questions_id", $big_ID);
+            $stmt->bindParam(":question_num", $question_num);
+            $stmt->bindParam(":user_answer", $user_answer);
+            $stmt->bindParam(":result", $result);
+            $stmt->execute();
+            // $dbh->insertRecord($answer_sql,$answer_bind_array);
+        }
+    }
+     catch(PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
 }
